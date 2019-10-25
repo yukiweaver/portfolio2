d9b710c779207dc31d7a33c8a8d2e607cd5652a5
yuki ueno <yuki.weaver1204@gmail.com>
2019-10-25 23:34:16 +0900

    トークルームレイアウト修正

--- app/service/create_notification.rb
+++ app/service/create_notification.rb
@@ -30,8 +30,8 @@ class CreateNotification
   def body
     {
       'app_id' => '97cc47fd-6eb0-4d36-ab4e-10948fa02037', # app_id
-      'url'    => 'https://rails-portfolio2.herokuapp.com',
-      # 'url'    => 'http://localhost:3000',
+      # 'url'    => 'https://rails-portfolio2.herokuapp.com',
+      'url'    => 'http://localhost:3000',
       'data'   => { 'type': type },
       'included_segments' => ['All'],
       'contents' => contents
